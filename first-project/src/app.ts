fafa6351b1d09f933aa354b76bcef8d207a5e58b
Safa Asgar <safaasgar58@gmail.com>
2023-12-06 03:30:02 +0600

    Handle Unhandled Rejection and Uncaught Exception

--- first-project/src/app.ts
+++ first-project/src/app.ts
@@ -15,9 +15,13 @@ app.use(cors())
 //application routes
 app.use('/api/v1', router)
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+const test = async (req: Request, res: Response) => {
+  // Promise.reject()
+  const a = 10
+  res.send(a)
+}
+
+app.get('/', test)
 
 app.use(globalErrorHandler)
 
--- first-project/src/server.ts
+++ first-project/src/server.ts
@@ -1,12 +1,15 @@
 import app from './app'
 import config from './app/config'
 import mongoose from 'mongoose'
+import { Server } from 'http'
+
+let server: Server
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`)
     })
   } catch (error) {
@@ -15,3 +18,18 @@ async function main() {
 }
 
 main()
+
+process.on('unhandledRejection', () => {
+  console.log(`💀 UnhandledRejection is Detected, shutting down....`)
+  if (server) {
+    server.close(() => {
+      process.exit()
+    })
+  }
+  process.exit(1)
+})
+
+process.on('uncaughtException', () => {
+  console.log(`⚡ Uncaught Exception is Detected, shutting down....`)
+  process.exit(1)
+})
