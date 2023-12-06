37583bded3ac97598375e64a549bb1ebbd13428a
Safa Asgar <safaasgar58@gmail.com>
2023-12-07 04:37:31 +0600

    Querybuilder Class added

--- first-project/src/app/modules/user/user.controller.ts
+++ first-project/src/app/modules/user/user.controller.ts
@@ -4,7 +4,7 @@ import sendResponse from '../../utils/sendResponse'
 import httpStatus from 'http-status'
 import catchAsync from '../../utils/catchAsync'
 
-const createStudent: RequestHandler = catchAsync(async (req, res, next) => {
+const createStudent: RequestHandler = catchAsync(async (req, res) => {
   const { password, student: studentData } = req.body
   const result = await UserServices.createStudentIntoDB(password, studentData)
 
