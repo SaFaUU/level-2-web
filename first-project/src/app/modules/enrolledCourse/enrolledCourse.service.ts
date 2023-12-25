9047f5380fe1fd99220ab472f92fff22673c8c1a
Safa Asgar <safaasgar58@gmail.com>
2023-12-25 12:52:37 +0600

    Create Enroll Course API

---
+++ first-project/src/app/modules/enrolledCourse/enrolledCourse.service.ts
@@ -0,0 +1,144 @@
+import httpStatus from 'http-status'
+import AppError from '../../errors/AppError'
+import { OfferedCourse } from '../offeredCourse/offeredCourse.model'
+import { TEnrolledCourse } from './enrolledCourse.interface'
+import EnrolledCourse from './enrolledCourse.model'
+import Student from '../student/student.model'
+import mongoose from 'mongoose'
+import { SemesterRegistration } from '../semesterRegistration/semesterRegistration.model'
+import { Course } from '../course/course.model'
+
+const createEnrolledCourseIntoDB = async (
+  userId: string,
+  payload: TEnrolledCourse,
+) => {
+  // Check if the course is offered
+  const { offeredCourse } = payload
+  const isOfferedCourseExists = await OfferedCourse.findById(offeredCourse)
+
+  if (!isOfferedCourseExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Offered Course not found')
+  }
+
+  if (isOfferedCourseExists.maxCapacity <= 0) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Course is full')
+  }
+
+  const student = await Student.findOne(
+    {
+      id: userId,
+    },
+    {
+      _id: 1,
+    },
+  )
+
+  if (!student) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Student not found')
+  }
+
+  const isStudentAlreadyEnrolled = await EnrolledCourse.findOne({
+    semesterRegistration: isOfferedCourseExists.semesterRegistration,
+    offeredCourse,
+    student: student._id,
+  })
+
+  if (isStudentAlreadyEnrolled) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Student already enrolled')
+  }
+
+  // check total credits exceeds maxCredit
+  const course = await Course.findById(isOfferedCourseExists.course)
+  const currentCredit = course?.credits
+  const semesterRegistration = await SemesterRegistration.findById(
+    isOfferedCourseExists.semesterRegistration,
+  ).select('maxCredit')
+
+  const maxCredit = semesterRegistration?.maxCredit
+
+  const enrolledCourses = await EnrolledCourse.aggregate([
+    {
+      $match: {
+        semesterRegistration: isOfferedCourseExists.semesterRegistration,
+        student: student._id,
+      },
+    },
+    {
+      $lookup: {
+        from: 'courses',
+        localField: 'course',
+        foreignField: '_id',
+        as: 'enrolledCourseData',
+      },
+    },
+    {
+      $unwind: '$enrolledCourseData',
+    },
+    {
+      $group: {
+        _id: null,
+        totalEnrolledCredits: {
+          $sum: '$enrolledCourseData.credits',
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+      },
+    },
+  ])
+  // total enrolled credits + new enrolled course credit > maxCredit
+  const totalCredits =
+    enrolledCourses.length > 0 ? enrolledCourses[0].totalEnrolledCredits : 0
+
+  if (totalCredits && maxCredit && totalCredits + currentCredit > maxCredit) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      'Total credits exceeds max credit',
+    )
+  }
+  const session = await mongoose.startSession()
+  try {
+    session.startTransaction()
+
+    //create
+    const result = await EnrolledCourse.create(
+      [
+        {
+          semesterRegistration: isOfferedCourseExists.semesterRegistration,
+          academicSemester: isOfferedCourseExists.academicSemester,
+          academicFaculty: isOfferedCourseExists.academicFaculty,
+          academicDepartment: isOfferedCourseExists.academicDepartment,
+          offeredCourse,
+          course: isOfferedCourseExists.course,
+          student: student._id,
+          faculty: isOfferedCourseExists.faculty,
+          isEnrolled: true,
+        },
+      ],
+      { session },
+    )
+    if (!result) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Course not enrolled')
+    }
+
+    const maxCapacity = isOfferedCourseExists.maxCapacity
+
+    await OfferedCourse.findByIdAndUpdate(offeredCourse, {
+      maxCapacity: maxCapacity - 1,
+    })
+
+    await session.commitTransaction()
+    await session.endSession()
+    return result
+  } catch (error: unknown) {
+    await session.abortTransaction()
+    await session.endSession()
+    throw new AppError(error.statusCode, error.message)
+  }
+}
+
+export const EnrolledCourseService = {
+  createEnrolledCourseIntoDB,
+}
--- first-project/src/app/modules/user/user.service.ts
+++ first-project/src/app/modules/user/user.service.ts
@@ -83,7 +83,11 @@ const createStudentIntoDB = async (
     throw new Error('Failed to create student')
   }
 }
-const createFacultyIntoDB = async (password: string, payload: TFaculty) => {
+const createFacultyIntoDB = async (
+  file: any,
+  password: string,
+  payload: TFaculty,
+) => {
   // create a user object
   const userData: Partial<TUser> = {}
 
@@ -110,6 +114,11 @@ const createFacultyIntoDB = async (password: string, payload: TFaculty) => {
     //set  generated id
     userData.id = await generateFacultyId()
 
+    const imageName = `${userData.id}${payload.name.firstName}`
+    const path = file?.path
+    // send image to cloudinary
+    const { secure_url } = await sendImageToCloudinary(imageName, path)
+
     // create a user (transaction-1)
     const newUser = await User.create([userData], { session }) // array
 
@@ -120,6 +129,7 @@ const createFacultyIntoDB = async (password: string, payload: TFaculty) => {
     // set id , _id as user
     payload.id = newUser[0].id
     payload.user = newUser[0]._id //reference _id
+    payload.profileImg = secure_url
 
     // create a faculty (transaction-2)
 
@@ -141,7 +151,11 @@ const createFacultyIntoDB = async (password: string, payload: TFaculty) => {
   }
 }
 
-const createAdminIntoDB = async (password: string, payload: TFaculty) => {
+const createAdminIntoDB = async (
+  file: any,
+  password: string,
+  payload: TFaculty,
+) => {
   // create a user object
   const userData: Partial<TUser> = {}
 
@@ -159,6 +173,11 @@ const createAdminIntoDB = async (password: string, payload: TFaculty) => {
     //set  generated id
     userData.id = await generateAdminId()
 
+    const imageName = `${userData.id}${payload.name.firstName}`
+    const path = file?.path
+    // send image to cloudinary
+    const { secure_url } = await sendImageToCloudinary(imageName, path)
+
     // create a user (transaction-1)
     const newUser = await User.create([userData], { session })
 
@@ -169,6 +188,7 @@ const createAdminIntoDB = async (password: string, payload: TFaculty) => {
     // set id , _id as user
     payload.id = newUser[0].id
     payload.user = newUser[0]._id //reference _id
+    payload.profileImg = secure_url
 
     // create a admin (transaction-2)
     const newAdmin = await Admin.create([payload], { session })
