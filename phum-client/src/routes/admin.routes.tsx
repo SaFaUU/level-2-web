37ffe1b541d02a5925b3fca605e728a185ddfbcd
Safa Asgar <safaasgar58@gmail.com>
2024-01-18 12:31:17 +0600

    Admin Route Setup

---
+++ phum-client/src/routes/admin.routes.tsx
@@ -0,0 +1,51 @@
+import AdminDashBoard from "../pages/admin/AdminDashBoard";
+import CreateAdmin from "../pages/admin/CreateAdmin";
+import CreateFaculty from "../pages/admin/CreateFaculty";
+import CreateStudent from "../pages/admin/CreateStudent";
+
+const adminPaths2 = [
+  {
+    name: "Dashboard",
+    path: "/admin/dashboard",
+    element: <AdminDashBoard />,
+  },
+  {
+    name: "User Management",
+    children: [
+      {
+        name: "Create Admin",
+        path: "/admin/create-admin",
+        element: <CreateAdmin />,
+      },
+      {
+        name: "Create Faculty",
+        path: "/admin/create-faculty",
+        element: <CreateFaculty />,
+      },
+      {
+        name: "Create Student",
+        path: "/admin/create-student",
+        element: <CreateStudent />,
+      },
+    ],
+  },
+];
+
+export const adminPaths = [
+  {
+    path: "dashboard",
+    element: <AdminDashBoard></AdminDashBoard>,
+  },
+  {
+    path: "create-student",
+    element: <CreateStudent></CreateStudent>,
+  },
+  {
+    path: "create-admin",
+    element: <CreateAdmin></CreateAdmin>,
+  },
+  {
+    path: "create-faculty",
+    element: <CreateFaculty></CreateFaculty>,
+  },
+];
