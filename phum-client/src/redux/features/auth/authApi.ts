2f7fc87bb347488dd2df34f07a8bb1afd4a2f854
Safa Asgar <safaasgar58@gmail.com>
2024-01-21 02:33:05 +0600

    Modular API for Authentication with RTK Query

--- phum-client/src/redux/features/auth/authApi.ts
+++ phum-client/src/redux/features/auth/authApi.ts
@@ -1,1 +1,15 @@
-const authApi = 
+import { baseApi } from "../../api/baseApi";
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (userInfo) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: userInfo,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = authApi;
