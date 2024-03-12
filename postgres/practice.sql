c1c400a1e37cd32a2bf99728336225030ac3f701
Safa Asgar <safaasgar58@gmail.com>
2024-03-12 11:45:18 +0600

    Creating Sub Queries and Views.

--- postgres/practice.sql
+++ postgres/practice.sql
@@ -48,3 +48,36 @@ SELECT department_name, round(avg(salary)) as avg_salary from employees join dep
 
 
 SELECT extract(year from hire_date), count(*) from employees join departments using(department_id) group by extract(year from hire_date);
+
+
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10, 2)
+);
+
+INSERT INTO orders (customer_id, order_date, total_amount) VALUES 
+    (1, '2022-01-05', 100.50),
+    (2, '2020-01-07', 200.75),
+    (1, '2022-01-08', 150.25),
+    (3, '2020-05-10', 300.00),
+    (2, '2022-01-15', 180.50),
+    (3, '2022-01-20', 220.25),
+    (1, '2022-01-25', 90.00),
+    (2, '2022-01-28', 120.75),
+    (3, '2021-02-01', 250.50),
+    (1, '2022-02-05', 180.25),
+    (4, '2023-02-05', 190.25);
+
+
+    -- Find customers who have placed more than 2 orders and calculate the total amount spent by each of these customers.
+
+select customer_id, count(*) from orders GROUP BY orders.customer_id;
+
+ -- Find the total amount of orders placed each month in the year 2022.
+
+SELECT extract(month from order_date), count(*) from orders where extract(year from order_date) = 2022 group by extract(month from order_date);
+
+
