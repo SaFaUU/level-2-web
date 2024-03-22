74e080a55098e30bce85b7780b66d0ae4e29f64c
Safa Asgar <safaasgar58@gmail.com>
2024-03-23 03:23:09 +0600

    Admin Creation API Created

--- PH-Healthcare/PH-HEALTHCARE-SERVER/src/app.ts
+++ PH-Healthcare/PH-HEALTHCARE-SERVER/src/app.ts
@@ -1,14 +1,21 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { userRoutes } from "./app/modules/User/user.routes";
 
 const app: Application = express();
 
 app.use(cors());
+
+// Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
     message: "Hello World!",
   });
 });
+
+app.use("/api/v1/user", userRoutes);
 
 export default app;
