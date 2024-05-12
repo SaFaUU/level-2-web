1b133605cfd75496b85e4482abb72e4bbf7df6a5
Safa Asgar <safaasgar58@gmail.com>
2024-05-12 19:45:07 +0600

    Next app with MUI Initialization

---
+++ PH-Healthcare/ph-healthcare-frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <Button variant="text">Text</Button>
+    </div>
+  );
+};
+
+export default HomePage;
