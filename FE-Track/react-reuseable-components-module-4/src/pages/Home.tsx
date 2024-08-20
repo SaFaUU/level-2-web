29b7865acbcba2f5c6e1d04f7e35da5a2076dfd6
Safa Asgar <safaasgar58@gmail.com>
2024-08-21 01:05:57 +0600

    Tailwind Class Merge Issues Fix

--- FE-Track/react-reuseable-components-module-4/src/pages/Home.tsx
+++ FE-Track/react-reuseable-components-module-4/src/pages/Home.tsx
@@ -1,9 +1,24 @@
+import clsx from "clsx";
 import React from "react";
+import { twMerge } from "tailwind-merge";
+import cn from "../utils/cn";
 
 const Home = () => {
   return (
     <div>
-      <button className="btn"> Home </button>
+      <button
+        className={cn(
+          "btn btn-primary",
+          {
+            "btn-disabled": false,
+            "btn-success": true,
+          },
+          "btn-warning"
+        )}
+      >
+        {" "}
+        Home{" "}
+      </button>
     </div>
   );
 };
