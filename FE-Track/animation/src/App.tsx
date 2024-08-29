b752ad462971a7098c5fac0c4994a58840b25910
Safa Asgar <safaasgar58@gmail.com>
2024-08-30 01:18:38 +0600

    CSS Basic Animations

---
+++ FE-Track/animation/src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main">
+      {/* <div className="box"></div> */}
+      {/* {/* <div className="box-1"></div> */}
+      {/* <div className="box-2"></div> */}
+      {/* <h1>Something Cool</h1> */}
+      {/* <button className="btn">Hover me</button> */}
+
+      <div className="loading">
+        <div className="glass"></div>
+        <div className="box"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
